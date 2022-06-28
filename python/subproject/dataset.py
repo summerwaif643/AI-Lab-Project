66dcbe4ecd4149f1ecf27e8000e81115e1d6fdba
summerwaif643 <summerwaif643@gmail.com>
2022-06-28 19:02:00 +0200

    added dataset

--- python/subproject/dataset.py
+++ python/subproject/dataset.py
@@ -1,5 +1,8 @@
 import os 
-from IPython.display import display, Image
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+
 '''
 We have two subdirectories
 Train/ which contains a lot of images
@@ -7,3 +10,9 @@ Train/ which contains a lot of images
 
 '''
 
+images = np.load("images/ab/ab/ab1.npy")
+im = Image.fromarray(images[0])
+#plt.imshow(images[0], cmap='gray')
+plt.show()
+im.show()
+#print(images)
