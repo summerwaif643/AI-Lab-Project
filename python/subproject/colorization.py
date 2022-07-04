4dda97159f3e66342066210745df4d65d8645fb0
summerwaif643 <summerwaif643@gmail.com>
2022-07-04 15:12:34 +0200

    final before byebye ubuntu

--- python/subproject/colorization.py
+++ python/subproject/colorization.py
@@ -30,21 +30,21 @@ class ColorizationNet(nn.Module):
         self.upsample = nn.Sequential(
             nn.Conv2d(MIDLEVEL_FEATURE_SIZE, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
-            nn.Mish(),
+            nn.ReLU(),
 
             nn.Upsample(scale_factor=2),
             nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(64),
-            nn.Mish(),
+            nn.ReLU(),
 
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(64),
-            nn.Mish(),
+            nn.ReLU(),
 
             nn.Upsample(scale_factor=2),
             nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
-            nn.Mish(),
+            nn.ReLU(),
             
             nn.Conv2d(32, 2, kernel_size=3, stride=1, padding=1),
             nn.Upsample(scale_factor=2)
